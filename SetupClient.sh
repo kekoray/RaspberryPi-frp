1760fcaba6c3809dc5b1bb8376c6320b1921e1da
kekoray <61939347+kekoray@users.noreply.github.com>
2021-04-30 14:54:28 +0800

    一键安装脚本

---
+++ SetupClient.sh
@@ -0,0 +1,10 @@
+latestVersion=0.36.2
+mkdir -p /tmp/frp
+wget -O /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${latestVersion}/frp_${latestVersion}_linux_arm.tar.gz
+tar -C /tmp/frp -zxvf /tmp/frp/frp.tar.gz
+cp /tmp/frp/frp_${latestVersion}_linux_arm/frpc /usr/bin
+chmod +x /usr/bin/frpc
+mkdir -p /etc/frp
+wget -O /etc/frp/frpc.ini https://github.com/kekoray/RaspberryPi-frp/blob/main/frpc.ini
+wget -O /etc/systemd/system/frpc.service https://github.com/kekoray/RaspberryPi-frp/blob/main/frpc.service
+rm -r /tmp/frp
---
+++ SetupServer.sh
@@ -0,0 +1,10 @@
+latestVersion=0.36.2
+mkdir -p /tmp/frp
+wget -O /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${latestVersion}/frp_${latestVersion}_linux_amd64.tar.gz
+tar -C /tmp/frp -zxvf /tmp/frp/frp.tar.gz
+cp /tmp/frp/frp_${latestVersion}_linux_amd64/frps /usr/bin
+chmod +x /usr/bin/frps
+mkdir -p /etc/frp
+wget -O /etc/frp/frps.ini https://github.com/kekoray/RaspberryPi-frp/blob/main/frps.ini
+wget -O /etc/systemd/system/frps.service https://github.com/kekoray/RaspberryPi-frp/blob/main/frps.service
+rm -r /tmp/frp
