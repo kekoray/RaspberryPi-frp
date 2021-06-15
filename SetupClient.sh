6eb856ce577f2c596b39a8998debc58aca3b3ac2
koray <347466123@qq.com>
2021-06-15 19:00:44 +0800

    fix

--- SetupClient.sh
+++ SetupClient.sh
@@ -5,6 +5,6 @@ tar -C /tmp/frp -zxvf /tmp/frp/frp.tar.gz
 cp /tmp/frp/frp_${latestVersion}_linux_arm/frpc /usr/bin
 chmod +x /usr/bin/frpc
 mkdir -p /etc/frp
-wget -O /etc/frp/frpc.ini https://raw.githubusercontent.com/kekoray/RaspberryPi-frp/blob/main/frpc.ini
-wget -O /etc/systemd/system/frpc.service https://raw.githubusercontent.com/kekoray/RaspberryPi-frp/blob/main/frpc.service
+wget -O /etc/frp/frpc.ini https://raw.githubusercontent.com/kekoray/RaspberryPi-frp/main/frpc.ini
+wget -O /etc/systemd/system/frpc.service https://raw.githubusercontent.com/kekoray/RaspberryPi-frp/main/frpc.service
 rm -r /tmp/frp
